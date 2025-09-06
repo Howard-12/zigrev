cde08f14e773000042b5784db0f0762150530557
Howard-12 <s0989175085@gmail.com>
2025-09-06 15:19:18 +1000

    feat: ui style

--- src/zigrev.zig
+++ src/zigrev.zig
@@ -56,6 +56,8 @@ pub fn setup(config: Config) !Self {
     _ = c.CIMGUI_CHECKVERSION();
     _ = c.ImGui_CreateContext(null);
 
+    imgui_style();
+
     const io = c.ImGui_GetIO();
     io.*.ConfigFlags |= c.ImGuiConfigFlags_NavEnableKeyboard;
     io.*.ConfigFlags |= c.ImGuiConfigFlags_DockingEnable;
@@ -121,3 +123,69 @@ pub fn clean(self: *Self) void {
     c.ImGui_DestroyContext(null);
 }
 
+fn imgui_style() void {
+        var style: *c.ImGuiStyle = c.ImGui_GetStyle();
+	var colours: *[60]c.ImVec4_t = &style.*.Colors;
+
+	colours[c.ImGuiCol_Text]                   = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 1.000};
+	colours[c.ImGuiCol_TextDisabled]           = c.ImVec4{ .x = 0.500, .y = 0.500, .z = 0.500, .w = 1.000};
+	colours[c.ImGuiCol_WindowBg]               = c.ImVec4{ .x = 0.180, .y = 0.180, .z = 0.180, .w = 1.000};
+	colours[c.ImGuiCol_ChildBg]                = c.ImVec4{ .x = 0.280, .y = 0.280, .z = 0.280, .w = 0.000};
+	colours[c.ImGuiCol_PopupBg]                = c.ImVec4{ .x = 0.313, .y = 0.313, .z = 0.313, .w = 1.000};
+	colours[c.ImGuiCol_Border]                 = c.ImVec4{ .x = 0.266, .y = 0.266, .z = 0.266, .w = 1.000};
+	colours[c.ImGuiCol_BorderShadow]           = c.ImVec4{ .x = 0.000, .y = 0.000, .z = 0.000, .w = 0.000};
+	colours[c.ImGuiCol_FrameBg]                = c.ImVec4{ .x = 0.160, .y = 0.160, .z = 0.160, .w = 1.000};
+	colours[c.ImGuiCol_FrameBgHovered]         = c.ImVec4{ .x = 0.200, .y = 0.200, .z = 0.200, .w = 1.000};
+	colours[c.ImGuiCol_FrameBgActive]          = c.ImVec4{ .x = 0.280, .y = 0.280, .z = 0.280, .w = 1.000};
+	colours[c.ImGuiCol_TitleBg]                = c.ImVec4{ .x = 0.148, .y = 0.148, .z = 0.148, .w = 1.000};
+	colours[c.ImGuiCol_TitleBgActive]          = c.ImVec4{ .x = 0.148, .y = 0.148, .z = 0.148, .w = 1.000};
+	colours[c.ImGuiCol_TitleBgCollapsed]       = c.ImVec4{ .x = 0.148, .y = 0.148, .z = 0.148, .w = 1.000};
+	colours[c.ImGuiCol_MenuBarBg]              = c.ImVec4{ .x = 0.195, .y = 0.195, .z = 0.195, .w = 1.000};
+	colours[c.ImGuiCol_ScrollbarBg]            = c.ImVec4{ .x = 0.160, .y = 0.160, .z = 0.160, .w = 1.000};
+	colours[c.ImGuiCol_ScrollbarGrab]          = c.ImVec4{ .x = 0.277, .y = 0.277, .z = 0.277, .w = 1.000};
+	colours[c.ImGuiCol_ScrollbarGrabHovered]   = c.ImVec4{ .x = 0.300, .y = 0.300, .z = 0.300, .w = 1.000};
+	colours[c.ImGuiCol_ScrollbarGrabActive]    = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_CheckMark]              = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 1.000};
+	colours[c.ImGuiCol_SliderGrab]             = c.ImVec4{ .x = 0.391, .y = 0.391, .z = 0.391, .w = 1.000};
+	colours[c.ImGuiCol_SliderGrabActive]       = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_Button]                 = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.000};
+	colours[c.ImGuiCol_ButtonHovered]          = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.156};
+	colours[c.ImGuiCol_ButtonActive]           = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.391};
+	colours[c.ImGuiCol_Header]                 = c.ImVec4{ .x = 0.313, .y = 0.313, .z = 0.313, .w = 1.000};
+	colours[c.ImGuiCol_HeaderHovered]          = c.ImVec4{ .x = 0.469, .y = 0.469, .z = 0.469, .w = 1.000};
+	colours[c.ImGuiCol_HeaderActive]           = c.ImVec4{ .x = 0.469, .y = 0.469, .z = 0.469, .w = 1.000};
+	colours[c.ImGuiCol_SeparatorHovered]       = c.ImVec4{ .x = 0.391, .y = 0.391, .z = 0.391, .w = 1.000};
+	colours[c.ImGuiCol_SeparatorActive]        = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_ResizeGrip]             = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.250};
+	colours[c.ImGuiCol_ResizeGripHovered]      = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.670};
+	colours[c.ImGuiCol_ResizeGripActive]       = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_Tab]                    = c.ImVec4{ .x = 0.098, .y = 0.098, .z = 0.098, .w = 1.000};
+	colours[c.ImGuiCol_TabHovered]             = c.ImVec4{ .x = 0.352, .y = 0.352, .z = 0.352, .w = 1.000};
+	colours[c.ImGuiCol_TabActive]              = c.ImVec4{ .x = 0.195, .y = 0.195, .z = 0.195, .w = 1.000};
+	colours[c.ImGuiCol_TabUnfocused]           = c.ImVec4{ .x = 0.098, .y = 0.098, .z = 0.098, .w = 1.000};
+	colours[c.ImGuiCol_TabUnfocusedActive]     = c.ImVec4{ .x = 0.195, .y = 0.195, .z = 0.195, .w = 1.000};
+	colours[c.ImGuiCol_DockingPreview]         = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 0.781};
+	colours[c.ImGuiCol_DockingEmptyBg]         = c.ImVec4{ .x = 0.180, .y = 0.180, .z = 0.180, .w = 1.000};
+	colours[c.ImGuiCol_PlotLines]              = c.ImVec4{ .x = 0.469, .y = 0.469, .z = 0.469, .w = 1.000};
+	colours[c.ImGuiCol_PlotLinesHovered]       = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_PlotHistogram]          = c.ImVec4{ .x = 0.586, .y = 0.586, .z = 0.586, .w = 1.000};
+	colours[c.ImGuiCol_PlotHistogramHovered]   = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_TextSelectedBg]         = c.ImVec4{ .x = 1.000, .y = 1.000, .z = 1.000, .w = 0.156};
+	colours[c.ImGuiCol_DragDropTarget]         = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_NavHighlight]           = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_NavWindowingHighlight]  = c.ImVec4{ .x = 1.000, .y = 0.391, .z = 0.000, .w = 1.000};
+	colours[c.ImGuiCol_NavWindowingDimBg]      = c.ImVec4{ .x = 0.000, .y = 0.000, .z = 0.000, .w = 0.586};
+	colours[c.ImGuiCol_ModalWindowDimBg]       = c.ImVec4{ .x = 0.000, .y = 0.000, .z = 0.000, .w = 0.586};
+	colours[c.ImGuiCol_Separator]              = colours[c.ImGuiCol_Border];
+
+	style.ChildRounding = 4.0;
+	style.FrameBorderSize = 1.0;
+	style.FrameRounding = 2.0;
+	style.GrabMinSize = 7.0;
+	style.PopupRounding = 2.0;
+	style.ScrollbarRounding = 12.0;
+	style.ScrollbarSize = 13.0;
+	style.TabBorderSize = 1.0;
+	style.TabRounding = 0.0;
+	style.WindowRounding = 4.0;
+}
